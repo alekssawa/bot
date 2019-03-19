cb41afe9f753031c38a298fe5dbbca0a312ad2cc
alekssawa <38007759+alekssawa@users.noreply.github.com>
2019-03-19 16:08:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => 
-        {if (message.content === 'pig') {message.reply('pog');}
+        {if (message.content === 'pig') {message.channel.send('pog');}
     
 });
 
