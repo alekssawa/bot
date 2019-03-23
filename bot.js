3316c09cb06543888c8995d1ffe9d344e98122a6
alekssawa <38007759+alekssawa@users.noreply.github.com>
2019-03-23 10:35:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', message => {
     if (message.content === 'Ты уебан') {message.channel.send('Крути барабан!)');}
     if (message.content === 'Садись') {message.channel.send('Бан!)');}
     if (message.content === 'Кто виноват') {message.channel.send('Навальный!!)');}
-    if (message.content === 'music') {message.channel.send('-play https://www.youtube.com/playlist?list=PLSdfU8nTff5TkhpGvJaACJVpcgYzjNec_');}
+    if (message.content === 'music') {message.channel.send("-play https://www.youtube.com/playlist?list=PLSdfU8nTff5TkhpGvJaACJVpcgYzjNec_" + "\n" + "!!!load 4cce102d-3ce9-48d6-b298-21cb460f9c98 -- ТРЕШ!!:astonished:");}
 });
 
 // THIS  MUST  BE  THIS  WAY
