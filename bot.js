58fa8b5065ee202c5e891a1523f362b81bc7633a
alekssawa <38007759+alekssawa@users.noreply.github.com>
2019-09-29 20:29:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
- client.on('message', message => {
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
     if (message.content === 'pig') {message.channel.send('pog');}
     if (message.content === '#help') {message.channel.send("***Пиши это в ЧаТ)***"
                                                            + "\n" + "***==================***"
