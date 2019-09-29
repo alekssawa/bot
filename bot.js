43f15542e62b16f7f4a7374eeee576e8d0873937
alekssawa <38007759+alekssawa@users.noreply.github.com>
2019-09-29 20:31:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,16 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+async function execute(message, serverQueue) {
+ const args = message.content.split(' ');
+ const voiceChannel = message.member.voiceChannel;
+ if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+  const permissions =     voiceChannel.permissionsFor(message.client.user);
+ if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+  return message.channel.send('I need the permissions to join and   speak in your voice channel!');
+ }
+}
+
 client.on('message', message => {
     if (message.content === 'pig') {message.channel.send('pog');}
     if (message.content === '#help') {message.channel.send("***Пиши это в ЧаТ)***"
