3b8ee2d3c6b48358829cdccacc0aaafee4d32c39
babykiwii <38007759+alekssawa@users.noreply.github.com>
2021-10-07 18:24:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,29 +2,47 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const prefix = "^";
+const CLEAR_MESSAGES = '!clearMessages';
 	
-client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+bot.on('ready', () => {
+  console.log('ClearMessagesBot is Ready!');
+  bot.on('message', message => {
+    if (message.content == CLEAR_MESSAGES) {
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
+      // Check the following permissions before deleting messages:
+      //    1. Check if the user has enough permissions
+      //    2. Check if I have the permission to execute the command
 
- if (command === "ping")
- {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! Это сообщение имеет задержку ${timeTaken}ms.`);
- }
- if (command === "clear")
- {
-    message.channel.bulkDelete;
- }
-	
-	
+      if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        return;
+      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        return;
+      }
 
+      // Only delete messages if the channel type is TextChannel
+      // DO NOT delete messages in DM Channel or Group DM Channel
+      if (message.channel.type == 'text') {
+        message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+            messagesDeleted = messages.array().length; // number of messages deleted
+
+            // Logging the number of messages deleted on both the channel and console.
+            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+          })
+          .catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+          });
+      }
+    }
+  });
 });
-
 
 
 client.login(process.env.BOT_TOKEN);
