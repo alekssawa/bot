d08ee5de81b81f047e6bdf233a1ca491f4013a96
babykiwii <38007759+alekssawa@users.noreply.github.com>
2021-10-07 19:08:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,10 @@ client.on("message", function(message) {
     message.reply(`Pong! Это сообщение имеет задержку ${timeTaken}ms.`);
  }
 	
-    if (msg.content.toLowerCase().startsWith(prefix + "clearchat")) {
-        async function clear() {
+    if (command === "clear") 
+    {
+        async function clear() 
+	{
             msg.delete();
             const fetched = await msg.channel.fetchMessages({limit: 99});
             msg.channel.bulkDelete(fetched);
