3ed30b5dd9975d60029d3094729a55d767e179b2
babykiwii <38007759+alekssawa@users.noreply.github.com>
2021-10-07 19:12:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,20 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
 const prefix = "^";
-	
-client.on("message", function(message) {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
-	
- if (command === "ping")
- {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply(`Pong! Это сообщение имеет задержку ${timeTaken}ms.`);
- }
-	
-
+client.on("ready" () => {
+    console.log("Successfully logged into client.");
 });
 
+client.on("message", msg => {
+    if (msg.content.toLowerCase().startsWith(prefix + "clearchat")) {
+        async function clear() {
+            msg.delete();
+            const fetched = await msg.channel.fetchMessages({limit: 99});
+            msg.channel.bulkDelete(fetched);
+        }
+        clear();
+    }
+});
 
-
-client.login(process.env.BOT_TOKEN);
+client.login("BOT_TOKEN");
