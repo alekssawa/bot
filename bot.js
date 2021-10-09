f1f42527588c5d997d5bc73faf8241017454bd4d
babykiwii <38007759+alekssawa@users.noreply.github.com>
2021-10-09 16:31:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,19 @@ client.on("ready" () => {
     console.log("Successfully logged into client.");
 });
 
-client.on("message", msg => {
-    if (msg.content.toLowerCase().startsWith(prefix + "clearchat")) {
+client.on("message", function(message) {
+    
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+    
+    if (command === "ping")
+ {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! Это сообщение имеет задержку ${timeTaken}ms.`);
+ }
+    
+    if (command === "clear") {
         async function clear() {
             msg.delete();
             const fetched = await msg.channel.fetchMessages({limit: 99});
