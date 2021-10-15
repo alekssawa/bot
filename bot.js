c61f17eb2d84cd1603b93b35d092847a3fb84783
babykiwii <38007759+alekssawa@users.noreply.github.com>
2021-10-15 19:22:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const prefix = "^";
+const prefix = "^^";
 	
 client.on("message", function(message) {
   if (message.author.bot) return;
